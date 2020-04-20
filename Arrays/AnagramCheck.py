caf83277cb4b2b8949d8ff3d3f210e17089bdaf3
mushtaqmahboob <mushtaq.akram@gmail.com>
2020-04-20 16:49:01 +0530

    Using sorted and replace

--- Arrays/AnagramCheck.py
+++ Arrays/AnagramCheck.py
@@ -1,3 +1,9 @@
-def anagram():
-    print("just testing the git repo")
+def anagram(s1,s2):
+
+    a1 = sorted(s1.replace(' ','').lower())
+    a2 = sorted(s1.replace(' ','').lower())
+
+    return a1 == a2
+
+print(anagram('akram','akmra'))
 

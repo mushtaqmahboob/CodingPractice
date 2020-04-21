2d122f5ab0d528d6bdc8e9627be3dfcb21437453
mushtaqmahboob <mushtaq.akram@gmail.com>
2020-04-21 14:43:04 +0530

    Using zip function of python

--- Arrays/FindMissingElement.py
+++ Arrays/FindMissingElement.py
@@ -1,16 +1,16 @@
 
 def findmissing(array1,array2):
 
-    # using the set data structure
-    seen = set()
-    output = set()
+    # Sort the arrays and using the zip function of python
+    array1.sort()
+    array2.sort()
 
-    for ele in array1:
-        if ele in array2:
-            seen.add(ele)
-        else:
-            output.add(ele)
+    for num1,num2 in zip(array1,array2):
+        if num1 != num2:
+            return num1
 
-    return output
+    return array1[-1]
+
 
 print(findmissing([1,2,3,4,5],[4,5,2,3]))
+print(findmissing([10,20,40,30,99],[20,10,30,40]))

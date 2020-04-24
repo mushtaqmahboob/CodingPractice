eb434384203f471b70df7721d6dd31d4b521b69e
mushtaqmahboob <mushtaq.akram@gmail.com>
2020-04-24 17:58:26 +0530

    Code with out explanation

---
+++ LeetCode/VerifyingAlienDictionary.py
@@ -0,0 +1,43 @@
+
+class Solution:
+
+    def veifyAlienDictionary(self, words, order) -> bool:
+
+        records = []
+
+        for i in range(len(words)-1):
+            for j in range(min(len(words[i]),len(words[i+1]))):
+
+                if words[i][j]==words[i+1][j]:
+                    continue
+                records.append([words[i][j],words[i+1][j]])
+                break
+
+        if records == []:
+            return False
+        for record in records:
+            if order.index(record[0]) > order.index(record[1]):
+                return False
+        return True
+
+if __name__ == '__main__':
+    words = ["word", "world", "row"]
+    order = "worldabcefghijkmnpqstuvxyz"
+    S= Solution()
+    print(S.veifyAlienDictionary(words,order))
+
+    words2 = ["hello", "leetcode"]
+    order2 = "hlabcdefgijkmnopqrstuvwxyz"
+    print(S.veifyAlienDictionary(words2,order2))
+
+    words3=["ubg", "kwh"]
+    order3="qcipyamwvdjtesbghlorufnkzx"
+    print(S.veifyAlienDictionary(words3,order3))
+
+    words4 = ["apple", "app"]
+    order4 = "abcdefghijklmnopqrstuvwxyz"
+    print(S.veifyAlienDictionary(words4,order4))
+
+
+
+
